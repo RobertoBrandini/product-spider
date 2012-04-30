9f0a8dd3bbdece49e3d9b29fa61aff7e8ef0b0c0
moraes <moraes@itemcase.com>
2012-04-30 17:15:20 -0300

    update

--- src/gps_config.py
+++ src/gps_config.py
@@ -1,10 +1,13 @@
 import urllib
 
 log_output_file = 'log/gps.log'
-
 gps_output_file = '/home/daniel/com.itemcase.spider/src/out/gps-laptop-query.html'
-
 local_gps_search_url = 'http://localhost/gps-laptop-query.html'
+curl_files = [
+    'http://localhost/spider/curl.php',     # local
+    'http://50.116.1.34/spider/curl.php',   # linode
+    'http://141.30.221.111:8787/curl.php'   # lopes
+]
 
 def mount_gps_search_url(query, page):
     gps_search_param = urllib.urlencode({
@@ -13,5 +16,4 @@ def mount_gps_search_url(query, page):
         'tbm': 'shop',
         'start': page*10-10
     })
-    req_url = 'http://50.116.1.34/spider/curl.php'
-    return req_url + '?url=%s' % ('https://www.google.com/search?' + gps_search_param).encode('base64', 'strict')
+    return curl_files[1] + '?url=%s' % ('https://www.google.com/search?' + gps_search_param).encode('base64', 'strict')
--- src/parser.py
+++ src/parser.py
@@ -1,13 +1,14 @@
 from gps_parser import GPSProductListParser
 from gps_config import *
 import datetime
+from pytz import timezone
 import urllib
 import pg
 
 parse_html = True
 collected = 0
 start_page = 1
-last_page = 100
+last_page = 3
 
 conn = pg.connect('itemcase', '50.116.1.34', 5432, None, None, 'postgres', 'fastMoving2BreakEverything')
 pgqueryobject = conn.query('SELECT cid_product FROM product')
@@ -16,24 +17,31 @@ result = pgqueryobject.getresult()
 log = open(log_output_file, 'a')
 
 if parse_html:
-    log.write(str(datetime.datetime.now()) + " - CID collection process started for " + str(last_page - start_page) + ' pages\n')
-    for i in range(start_page, last_page):
-        print 'Page: #' + str(i)
-        f = urllib.urlopen(mount_gps_search_url('laptop', i))
-        s = f.read().decode('UTF-8')
-        parser = GPSProductListParser()
-        parser.feed(s)
-	print parser.cid
-        for c in parser.cid:
-            if not (c,) in result:
-                result.append((c,))
-                collected += 1
-                log.write(str(datetime.datetime.now()) + " - CID #" + c + " collected\n")
-                conn.query('INSERT INTO product VALUES(' + c + ', \'' + str(datetime.date.today()) + '\')')
-        parser.close()    
-    
-    log.write(str(datetime.datetime.now()) + " - CID collection process finished\n")
-    log.write(str(datetime.datetime.now()) + " - " + str(collected) + " new CIDs was collected\n")
+    log.write(str(datetime.datetime.now(timezone('UTC'))) + " - CID collection process started for " + str(last_page - start_page) + ' pages\n')
+    try:
+        for i in range(start_page, last_page):
+            print 'Page: #' + str(i)
+            f = urllib.urlopen(mount_gps_search_url('laptop', i))
+            s = f.read().decode('UTF-8')
+            parser = GPSProductListParser()
+            parser.feed(s)
+            if len(parser.cid) == 0:
+                print s
+                log.write(str(datetime.datetime.now(timezone('UTC'))) + " - Request error\n")
+                break
+            print parser.cid
+            for c in parser.cid:
+                if not (c,) in result:
+                    result.append((c,))
+                    collected += 1
+                    log.write(str(datetime.datetime.now(timezone('UTC'))) + " - CID #" + c + " collected\n")
+                    conn.query('INSERT INTO product VALUES(' + c + ', \'' + str(datetime.date.today()) + '\')')
+            parser.close()
+    except:
+            print "Except"
+    finally:    
+        log.write(str(datetime.datetime.now(timezone('UTC'))) + " - CID collection process finished\n")
+        log.write(str(datetime.datetime.now(timezone('UTC'))) + " - " + str(collected) + " new CIDs was collected\n")
 
 log.close()
 conn.close()
