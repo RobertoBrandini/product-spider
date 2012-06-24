abf03ec1104bc3aa55a262c30c9e89aa6c8b29ec
moraes <moraes@itemcase.com>
2012-06-24 12:17:36 -0300

    bug fixes

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ pdataspider_logfile = 'log/pdataspider.log'
 gps_output_file = '/home/daniel/com.itemcase.spider/src/offline_request/gps-laptop-query.html'
 local_gps_search_url = 'http://localhost/gps-laptop-query.html'
 c_files = [
-    'http://localhost/spider/curl.php',                             # local
+    #'http://localhost/spider/curl.php',                             # local
     'http://50.116.1.34/spider/curl.php',                           # linode
     'http://www.personality-research.com/_moraes/curl.php',         # sonovitta
     'http://www.students.ic.unicamp.br/~ra123530/_spider/curl.php', # unicamp
