64b520878b9d933c4a61a57947cf7180e3e479e1
moraes <moraes@itemcase.com>
2012-07-30 23:15:13 -0300

    Adding 3 new bridges.

--- src/config.py
+++ src/config.py
@@ -5,21 +5,24 @@ gps_output_file = '/home/daniel/com.itemcase.spider/src/offline_request/gps-lapt
 local_gps_search_url = 'http://localhost/gps-laptop-query.html'
 c_files = [
     #'http://localhost/spider/curl.php',                             # local
-    'http://50.116.1.34/spider/curl.php',                           # linode
-    'http://www.personality-research.com/_moraes/curl.php',         # sonovitta
-    'http://www.students.ic.unicamp.br/~ra123530/_spider/curl.php', # unicamp
-    'http://www.vestibularseriado.com.br/curl.php',                 # vestibularseriado
-    'http://www.webulando.com.br/curl.php',                         # webulando    
-    'http://gpsc.comli.com/curl.php',                               # 000webhost
-    'http://gpsc1.site50.net/curl.php',                             # 000webhost
-    'http://www.gpsc.bugs3.com/curl.php',                           # serversfree
-    'http://gpsc.ueuo.com/curl.php',                                # freewebhostingarea
-    'http://gpsc.6te.net/curl.php',                                 # freewebhostingarea
-    'http://gpsc.orgfree.com/curl.php',                             # freewebhostingarea
-    'http://gpsc.noads.biz/curl.php',                               # freewebhostingarea
-    'http://gpsc.coolpage.biz/curl.php',                            # freewebhostingarea
-    'http://gpsc.freevar.com/curl.php',                             # freewebhostingarea
-    'http://gpsc.freeoda.com/curl.php',                             # freewebhostingarea
-    'http://141.30.221.111:8787/curl.php',                          # filipe
-    'http://dbm01.no-ip.org/curl.php'                               # rafael
+    'http://50.116.1.34/spider/curl.php',                           # linode                (50.116.1.34)
+    'http://www.personality-research.com/_moraes/curl.php',         # sonovitta             (75.119.205.176)
+    'http://www.students.ic.unicamp.br/~ra123530/_spider/curl.php', # unicamp               (143.106.16.162)
+    'http://www.vestibularseriado.com.br/curl.php',                 # vestibularseriado     (189.38.81.133)
+    'http://www.webulando.com.br/curl.php',                         # webulando             (96.126.101.215)
+    'http://gpsc.comli.com/curl.php',                               # 000webhost            (31.170.163.90)
+    'http://gpsc1.site50.net/curl.php',                             # 000webhost            (31.170.162.243)
+    'http://www.gpsc.bugs3.com/curl.php',                           # serversfree           (31.170.166.142)
+    'http://gpsc.ueuo.com/curl.php',                                # freewebhostingarea    (64.31.24.10)
+    'http://gpsc.6te.net/curl.php',                                 # freewebhostingarea    (74.63.255.142)
+    'http://gpsc.orgfree.com/curl.php',                             # freewebhostingarea    (78.47.28.69)
+    'http://gpsc.noads.biz/curl.php',                               # freewebhostingarea    (208.115.240.37)
+    'http://gpsc.coolpage.biz/curl.php',                            # freewebhostingarea    (208.115.247.203)
+    'http://gpsc.freevar.com/curl.php',                             # freewebhostingarea    (64.31.24.10)
+    'http://gpsc.freeoda.com/curl.php',                             # freewebhostingarea    (74.63.255.142)
+    'http://gpsc.my-php.net/curl.php',                              # my-php.net            (209.190.24.4)
+    'http://gpsc.sisko-hosting.com/curl.php',                       # sisko-hosting         (31.170.166.94)
+    'http://gpsc.3owl.com/curl.php'                                 # 3owl                  (31.170.166.106)
+    #'http://141.30.221.111:8787/curl.php',                          # filipe
+    #'http://dbm01.no-ip.org/curl.php'                               # rafael
 ]
